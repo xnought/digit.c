f06b3b80a7a580437592c777c83da9ca09219d63
xnought <drbertucci@hotmail.com>
2025-01-15 16:59:13 -0500

    print

--- gpt.c
+++ gpt.c
@@ -6,21 +6,44 @@
 typedef struct tensor
 {
 	int shape[DIMS_MAX];
+	float *data; // points to the 1d points of data
 } tensor;
 
 void tensor_print_shape(tensor *t)
 {
-	printf("Shape: ( ");
+	printf("Shape:\t");
 	for (int i = 0; i < DIMS_MAX; i++)
 	{
 		if (t->shape[i] != 0)
 			printf("%d ", t->shape[i]);
 	}
-	printf(")");
+	printf("\n");
+}
+int tensor_flat_length(tensor *t)
+{
+	int total = 1;
+	for (int i = 0; i < DIMS_MAX; i++)
+	{
+		if (t->shape[i] != 0)
+			total *= t->shape[i];
+	}
+	return total;
+}
+void tensor_print_data(tensor *t)
+{
+	printf("Data:\t");
+	for (int i = 0; i < tensor_flat_length(t); i++)
+	{
+		printf("%.2f ", t->data[i]);
+	}
+	printf("\n");
 }
 void tensor_print(tensor *t)
 {
+	printf("=====Tensor=====\n");
 	tensor_print_shape(t);
+	tensor_print_data(t);
+	printf("================\n");
 }
 
 void linear_regression_example()
@@ -29,7 +52,8 @@ void linear_regression_example()
 
 	printf("1. Define the dataset to do lin reg on.\n");
 	// Shaped (N points, D dimension). In this case D = 1. So just a vector.
-	tensor x = {.shape = {100, 1}};
+	float d[] = {1.0, 2.0, 3.0};
+	tensor x = {.shape = {3}, .data = d};
 	tensor_print(&x);
 }
 
