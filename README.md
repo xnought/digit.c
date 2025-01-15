d32a6e02247ad335eed3bf5fc4c2e7d44052d484
xnought <drbertucci@hotmail.com>
2025-01-15 16:47:27 -0500

    tensor print

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ To compile and run do
 ```
 
 or do `./run` which does that exact command.
+
+## TODO
+
+- [ ] Linear regression training example
+- [ ] Logistic regression training example 
+- [ ] MNIST MLP training example
+- [ ] Attention GPT training example
--- gpt.c
+++ gpt.c
@@ -1,15 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define DIMS_MAX 10
+
 typedef struct tensor
 {
-	int data;
+	int shape[DIMS_MAX];
 } tensor;
+
+void tensor_print_shape(tensor *t)
+{
+	printf("Shape: ( ");
+	for (int i = 0; i < DIMS_MAX; i++)
+	{
+		if (t->shape[i] != 0)
+			printf("%d ", t->shape[i]);
+	}
+	printf(")");
+}
+void tensor_print(tensor *t)
+{
+	tensor_print_shape(t);
+}
+
+void linear_regression_example()
+{
+	printf("Linear Regression Example.\n");
+
+	printf("1. Define the dataset to do lin reg on.\n");
+	// Shaped (N points, D dimension). In this case D = 1. So just a vector.
+	tensor x = {.shape = {100, 1}};
+	tensor_print(&x);
+}
 
 int main()
 {
-	printf("Hello GPT!\n");
-	tensor a = {.data = 1};
-	printf("(%d)\n", a.data);
+	linear_regression_example();
 	return 0;
 }
