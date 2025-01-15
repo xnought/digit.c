435bc65ee7f9dec344fd2e8fb93a79f2ada7c9ff
xnought <drbertucci@hotmail.com>
2025-01-15 16:30:34 -0500

    gptc

--- gpt.c
+++ gpt.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct tensor
+{
+	int data;
+} tensor;
+
 int main()
 {
 	printf("Hello GPT!\n");
+	tensor a = {.data = 1};
+	printf("(%d)\n", a.data);
 	return 0;
 }
