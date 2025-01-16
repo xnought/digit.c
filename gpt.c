8d8d2d29dfc900dc14e9742b38e72d66ee382464
xnought <drbertucci@hotmail.com>
2025-01-16 16:37:34 -0500

    forward pass

--- gpt.c
+++ gpt.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <math.h>
 
-#define DIMS_MAX 3
+#define SHAPE_MAX 2
 #define OPS_ARGS_MAX 2
+#define t_shape int[SHAPE_MAX]
 
 typedef struct tensor
 {
-	int shape[DIMS_MAX];
+	int shape[SHAPE_MAX];
 	float *data; // points to the 1d points of data
 	float *grad;
 	struct tensor *ops_args[OPS_ARGS_MAX]; // tensors used in an ops function args
+	int transposed;
 } tensor;
 
 void tensor_print_shape(tensor *t)
 {
 	printf("Shape:\t");
-	for (int i = 0; i < DIMS_MAX; i++)
+	for (int i = 0; i < SHAPE_MAX; i++)
 	{
 		if (t->shape[i] != 0)
 			printf("%d ", t->shape[i]);
@@ -26,7 +29,7 @@ void tensor_print_shape(tensor *t)
 int tensor_flat_length(tensor *t)
 {
 	int total = 1;
-	for (int i = 0; i < DIMS_MAX; i++)
+	for (int i = 0; i < SHAPE_MAX; i++)
 	{
 		if (t->shape[i] != 0)
 			total *= t->shape[i];
@@ -51,6 +54,21 @@ void tensor_print_grad(tensor *t)
 	}
 	printf("\n");
 }
+int tensor_index2d(tensor *t, int i, int j)
+{
+	return t->transposed ? j * t->shape[0] + i : i * t->shape[1] + j;
+}
+void tensor_print2d(tensor *m)
+{
+	for (int i = 0; i < m->shape[0]; i++)
+	{
+		for (int j = 0; j < m->shape[1]; j++)
+		{
+			printf("%0.2f ", m->data[tensor_index2d(m, i, j)]);
+		}
+		printf("\n");
+	}
+}
 #define tensor_print(tensor_pointer) (                    \
 	{                                                     \
 		printf("=====Tensor=====\n");                     \
@@ -69,6 +87,11 @@ float *tensor_malloc_data(int flat_length)
 tensor *tensor_malloc()
 {
 	tensor *t = malloc(sizeof(tensor));
+	for (int i = 0; i < OPS_ARGS_MAX; i++)
+	{
+		t->ops_args[i] = NULL;
+	}
+	t->transposed = 0;
 	return t;
 }
 void tensor_free(tensor *t)
@@ -77,16 +100,12 @@ void tensor_free(tensor *t)
 	free(t->grad);
 	free(t);
 }
-tensor *tensor_zeros(int shape[DIMS_MAX])
+tensor *tensor_zeros(int shape[SHAPE_MAX])
 {
 	tensor *t = tensor_malloc();
-	for (int i = 0; i < DIMS_MAX; i++)
+	for (int i = 0; i < SHAPE_MAX; i++)
 	{
 		t->shape[i] = shape[i];
-	}
-	for (int i = 0; i < OPS_ARGS_MAX; i++)
-	{
-		t->ops_args[i] = NULL;
 	}
 	int flat_length = tensor_flat_length(t);
 	t->data = tensor_malloc_data(flat_length);
@@ -101,14 +120,14 @@ tensor *tensor_zeros(int shape[DIMS_MAX])
 tensor *tensor_arange(float start, float stop, float step)
 {
 	float flat_length = (int)((stop - start) / step);
-	tensor *t = tensor_zeros((int[DIMS_MAX]){flat_length, 1});
+	tensor *t = tensor_zeros((t_shape){flat_length, 1});
 	for (int i = 0; i < flat_length; i++)
 	{
 		t->data[i] = start + i * step;
 	}
 	return t;
 }
-tensor *tensor_ones(int shape[DIMS_MAX])
+tensor *tensor_ones(int shape[SHAPE_MAX])
 {
 	tensor *t = tensor_zeros(shape);
 	for (int i = 0; i < tensor_flat_length(t); i++)
@@ -117,43 +136,44 @@ tensor *tensor_ones(int shape[DIMS_MAX])
 	}
 	return t;
 }
-
 void tensor_assert_same_shape(tensor *a, tensor *b)
 {
-	for (int i = 0; i < DIMS_MAX; i++)
+	for (int i = 0; i < SHAPE_MAX; i++)
 	{
 		assert(a->shape[i] == b->shape[i]);
 	}
 }
-
 tensor **tensor_malloc_ops_args(int num_ops)
 {
 	return malloc(num_ops * sizeof(tensor *));
 }
+void tensor_transpose(tensor *t)
+{
+	t->transposed = 1;
+	int temp = t->shape[1];
+	t->shape[1] = t->shape[0];
+	t->shape[0] = temp;
+}
 
 tensor *ops_sum(tensor *t)
 {
-	tensor *output = tensor_zeros((int[DIMS_MAX]){1, 1});
+	tensor *output = tensor_zeros((t_shape){1, 1});
 	for (int i = 0; i < tensor_flat_length(t); i++)
 	{
-		output->data[0] += t->data[i]; // forward
-		t->grad[i] = 1.0;			   // backward
+		output->data[0] += t->data[i];
 	}
 
 	output->ops_args[0] = t;
 
 	return output;
 }
-
 tensor *ops_add(tensor *a, tensor *b)
 {
 	tensor_assert_same_shape(a, b);
 	tensor *output = tensor_zeros(a->shape);
 	for (int i = 0; i < tensor_flat_length(a); i++)
 	{
-		output->data[i] = a->data[i] + b->data[i]; // forward
-		a->grad[i] = 1.0;						   // backward
-		b->grad[i] = 1.0;						   // backward
+		output->data[i] = a->data[i] + b->data[i];
 	}
 
 	output->ops_args[0] = a;
@@ -161,31 +181,47 @@ tensor *ops_add(tensor *a, tensor *b)
 
 	return output;
 }
-
 tensor *ops_square(tensor *t)
 {
 	tensor *output = tensor_zeros(t->shape);
 	for (int i = 0; i < tensor_flat_length(t); i++)
 	{
 		float t_i = t->data[i];
-		output->data[i] = t_i * t_i; // forward
-		t->grad[i] = 2 * t_i;		 // backward
+		output->data[i] = t_i * t_i;
 	}
 
 	output->ops_args[0] = t;
 
 	return output;
 }
-
 tensor *ops_sub(tensor *a, tensor *b)
 {
 	tensor_assert_same_shape(a, b);
 	tensor *output = tensor_zeros(a->shape);
 	for (int i = 0; i < tensor_flat_length(a); i++)
 	{
-		output->data[i] = a->data[i] - b->data[i]; // forward
-		a->grad[i] = 1.0;						   // backward
-		b->grad[i] = -1.0;						   // backward
+		output->data[i] = a->data[i] - b->data[i];
+	}
+
+	output->ops_args[0] = a;
+	output->ops_args[1] = b;
+
+	return output;
+}
+tensor *ops_matmul(tensor *a, tensor *b)
+{
+	assert(a->shape[1] == b->shape[0]);
+
+	tensor *output = tensor_zeros((t_shape){a->shape[0], b->shape[1]});
+	for (int i = 0; i < a->shape[0]; i++)
+	{
+		for (int j = 0; j < b->shape[1]; j++)
+		{
+			for (int k = 0; k < a->shape[1]; k++)
+			{
+				output->data[tensor_index2d(output, i, j)] += a->data[tensor_index2d(a, i, k)] * b->data[tensor_index2d(b, k, j)];
+			}
+		}
 	}
 
 	output->ops_args[0] = a;
@@ -201,7 +237,6 @@ tensor *loss_mse(tensor *a, tensor *b)
 	tensor *sub = ops_sub(a, b);
 	tensor *sqr = ops_square(sub);
 	tensor *sum = ops_sum(sqr);
-	sum->grad[0] = 1.0; // dloss/dloss = 1 obvi
 
 	return sum;
 }
@@ -268,22 +303,40 @@ void graph_backprop(tensor *node)
 	}
 }
 
+float rand_0_1()
+{
+	return (float)rand() / (float)RAND_MAX;
+}
+
+float rand_between(float a, float b)
+{
+	return a + rand_0_1() * (b - a);
+}
+
+void tensor_seed_random(int seed)
+{
+	srand(seed);
+}
+tensor *tensor_random(float a, float b, int shape[SHAPE_MAX])
+{
+	tensor *t = tensor_zeros(shape);
+	for (int i = 0; i < tensor_flat_length(t); i++)
+	{
+		t->data[i] = rand_between(a, b);
+	}
+	return t;
+}
+
 void linear_regression_example()
 {
-	printf("Linear Regression Example.\n");
+	tensor_seed_random(0);
 
-	printf("1. Define the dataset to do lin reg on.\n");
-	// Shaped (N points, D dimension). In this case D = 1. So just a vector.
-	// tensor *x = tensor_arange(0, 6, 1);
-	tensor *y = tensor_arange(0, 6, 1);
-	tensor *y_hat = tensor_ones(y->shape);
+	tensor *x = tensor_arange(0, 6, 1);				   // (N, d)
+	tensor *y = tensor_arange(0, 6, 1);				   // (N, 1)
+	tensor *w = tensor_random(-1, 1, (t_shape){1, 1}); // (d, 1)
 
-	tensor *loss = loss_mse(y, y_hat);
-	graph_backprop(loss);
-	tensor_print(y);
-	tensor_print(y_hat);
-	tensor_print(loss);
-	graph_free(loss);
+	tensor *yhat = ops_matmul(x, w);
+	tensor_print2d(yhat);
 }
 
 int main()
